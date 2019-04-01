d0aa410dbabd3d9d41c61944dea5d2f974332b30
Evelyn H <hobert.evelyn@gmail.com>
2019-04-02 00:17:18 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='iterchain',
+    version='0.0.1',
+    description='Iterator chaining for Python',
+    long_description='',
+    url='',
+    author='Evelyn H.',
+    author_email='hobert.evelyn@gmail.com',
+    packages=[],
+    classifiers=[
+      'Development Status :: 1 - Planning',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
