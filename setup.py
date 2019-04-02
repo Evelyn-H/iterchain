ddd43194dfed86880ee0845d66ce311ccba3c8b0
Evelyn_H <manu.hobert@gmail.com>
2019-04-02 19:45:25 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='iterchain',
-    version='0.0.1',
+    version='0.1.0',
     description='Iterator chaining for Python',
     long_description='',
     url='https://github.com/Evelyn-H/iterchain',
