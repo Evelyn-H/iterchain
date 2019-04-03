49d1e2f4b304da50016ae6d44957c30b8b1cadd5
Evelyn_H <manu.hobert@gmail.com>
2019-04-03 23:09:11 +0200

    Version bump (0.1.1)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='iterchain',
-    version='0.1.0',
+    version='0.1.1',
     description='Iterator chaining for Python',
     long_description=long_description,
     long_description_content_type="text/markdown",
